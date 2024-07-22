a0ad3a6f212e223c6eb46a9c37efa079472e1207
kypltIT <kyplt.it@gmail.com>
2024-07-23 02:35:48 +0700

    majors setup commit

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -9,5 +9,6 @@ namespace SIMS.Data
         public DbSet<SIMS.Models.Semesters> Semesters { get; set; } = default!;
         public DbSet<SIMS.Models.Departments> Departments { get; set; } = default!;
         public DbSet<SIMS.Models.Roles> Roles { get; set; } = default!;
+        public DbSet<SIMS.Models.Majors> Majors { get; set; } = default!;
     }
 }
