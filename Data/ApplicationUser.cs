753c4fc7ae7161aecef4493848f3d42d82686aab
kypltIT <kyplt.it@gmail.com>
2024-08-29 16:05:07 +0700

    fix test commit

--- Data/ApplicationUser.cs
+++ Data/ApplicationUser.cs
@@ -6,17 +6,14 @@ namespace SIMS.Data
     // Add profile data for application users by adding properties to the ApplicationUser class
     public class ApplicationUser : IdentityUser
     {
-		public string Code { get; set; }
-		public string FirstName { get; set; }
-		public string LastName { get; set; }
-	
-		public string Role { get; set; }
-/*		public DateTime DoB { get; set; }
-*//*		public string Address { get; set; }*/
-/*		public string Gender { get; set; }*/
+        public string Code { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
 
-		public string IntialAdmissionCourse { get; set; }
-		public string Status { get; set; }
-	}
+        public string Role { get; set; }
+
+        public string IntialAdmissionCourse { get; set; }
+        public string Status { get; set; }
+    }
 
 }
--- Program.cs
+++ Program.cs
@@ -40,7 +40,10 @@ namespace SIMS
 
             });
 
-            var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+            var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") 
+                ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+            
+            
             builder.Services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(connectionString));
 
